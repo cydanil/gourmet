8429bba11d6ebaec8a508ad7f6e2aa72991c91c6
Cyril Danilevski <cydanil@gmail.com>
2020-05-20 16:29:42 +0200

    test

--- gourmet/tests/old_databases_test.sh
+++
@@ -1,8 +0,0 @@
-cp -R old_databases /tmp/old_databases
-for folder in /tmp/old_databases/*
-do
-    echo Testing Gourmet\'s update from $folder
-    gourmet --gourmet-directory=$folder
-    echo Done with test.
-done
-rm -r /tmp/old_databases

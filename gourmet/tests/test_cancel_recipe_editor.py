d5be931f011d8a210c95d7820449481eca237058
Cyril Danilevski <cydanil@gmail.com>
2020-06-27 01:34:44 +0200

    Test passes when launched from pycharm

--- gourmet/tests/test_cancel_recipe_editor.py
+++ gourmet/tests/test_cancel_recipe_editor.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+import sys
 
-from dogtail.procedural import click, focus, keyCombo
+from dogtail import procedural
 from dogtail import tree
 from dogtail.utils import run
 
@@ -22,35 +23,35 @@ def test_cancel_edit_in_recipe_editor():
     assert gourmet is not None, "Could not find Gourmet instance!"
 
     # Get the first recipe in the list
-    focus.table()
-    keyCombo("<Up>")
+    procedural.focus.table()
+    procedural.keyCombo("<Up>")
 
     # Open it, and its recipe editor, to add ingredients
-    keyCombo("<Ctrl>o")
-    click("Edit ingredients")
+    procedural.keyCombo("<Ctrl>o")
+    procedural.click("Edit ingredients")
 
     # Focus on the recipe editor and add a new ingredient
-    focus.window("Fancy Recipe (Edit)")
-    focus.text()
-    type("1 cup flour")
-    keyCombo("<Enter>")
+    procedural.focus.window("Fancy Recipe (Edit)")
+    procedural.focus.text()
+    procedural.type("1 cup flour")
+    procedural.keyCombo("<Enter>")
 
     # Close the window and cancel when asked to save changes
-    keyCombo("<Alt><F4>")
-    click("Cancel")
+    procedural.keyCombo("<Alt><F4>")
+    procedural.click("Cancel")
 
     # Assert that the recipe editor is still open
-    focus.window("Fancy Recipe (Edit)")
+    procedural.focus.window("Fancy Recipe (Edit)")
 
     # Close the window using a different shortcut
-    keyCombo("<Ctrl>W")
+    procedural.keyCombo("<Ctrl>W")
     # Use a shortcut to validate saving, as dogtail does not find the button :/
-    keyCombo("<Alt>S")
+    procedural.keyCombo("<Alt>S")
 
     # There are now two windows, the recipe card, and main window
     # Close them successively to quit the application
-    keyCombo("<Alt><F4>")
-    keyCombo("<Alt><F4>")
+    procedural.keyCombo("<Alt><F4>")
+    procedural.keyCombo("<Alt><F4>")
 
 
 if __name__ == "__main__":
